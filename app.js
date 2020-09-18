3eb0a3d3dde114b81b951a163838cc988d0ec5f6
Gerardo <gerard.os.jr@gmail.com>
2020-09-18 17:51:19 -0500

    Trying static folder

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public', 'static')))
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.locals.title = 'Punta Piedra APP'

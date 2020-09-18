e114bc86734c901101fb23ff975511be0b7366e6
Gerardo <gerard.os.jr@gmail.com>
2020-09-18 16:58:31 -0500

    Testing front

--- app.js
+++ app.js
@@ -1,47 +1,32 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
+require('dotenv').config()
 
+let bodyParser = require('body-parser')
+let cookieParser = require('cookie-parser');
+let express = require('express');
+let favicon = require('serve-favicon')
+let logger = require('morgan');
+let path = require('path');
+let cors = require('cors')
 
-const app = express();
+var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(cors({
+  credentials: true,
+  origin: true
+}))
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-const index = require('./routes/index');
-app.use('/', index);
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.locals.title = 'Punta Piedra APP'
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
 module.exports = app;
---
+++ public/precache-manifest.60979484993aa356ef93f7ebae0c095b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c855e6b58e19424dba6962796403da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd2c5e229e459dd176bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd556cd9609b1bb848cd",
+    "url": "/static/js/2.97639ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97639ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2c5e229e459dd176bb",
+    "url": "/static/js/main.aeb0dd04.chunk.js"
+  },
+  {
+    "revision": "04bb83b710a2a9329cef",
+    "url": "/static/js/runtime-main.19c99f86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
